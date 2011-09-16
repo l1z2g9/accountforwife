0ea203400fcc9162088eb3be4094a3d6b9c740a4
querychinesesto <querychinesesto@localhost>
2011-09-16 17:51:54 +0800

    ''

--- mobile/src/cat/AccountPanelMIDlet.java
+++ mobile/src/cat/AccountPanelMIDlet.java
@@ -1,38 +1,23 @@
 package cat;
 
-import java.io.IOException;
-
 import javax.microedition.midlet.MIDlet;
 
 import com.sun.lwuit.Display;
-import com.sun.lwuit.plaf.UIManager;
-import com.sun.lwuit.util.Resources;
 
 public class AccountPanelMIDlet extends MIDlet {
 	private AccountPanel main = new AccountPanel();
 
 	public void startApp() {
 		Display.init(this);
-		String name = "TipsterTheme";
-		Resources r;
-		try {
-			r = Resources.open("/" + name + ".res");
-			UIManager.getInstance().setThemeProps(
-					r.getTheme(r.getThemeResourceNames()[0]));
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+		main.startApp();
 	}
 
 	public void pauseApp() {
-
+		main.pauseApp();
 	}
 
 	public void destroyApp(boolean unconditional) {
-
+		main.destroyApp(unconditional);
 	}
 
 }
