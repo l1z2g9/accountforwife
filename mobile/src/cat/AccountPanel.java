4745c6bde25ca2b49c37ed70dfa0066f2f6a8cb6
querychinesesto <querychinesesto@localhost>
2011-09-20 18:24:54 +0800

    use lwuit

--- mobile/src/cat/AccountPanel.java
+++ mobile/src/cat/AccountPanel.java
@@ -2,22 +2,35 @@ package cat;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Hashtable;
 
+import com.sun.lwuit.Button;
 import com.sun.lwuit.Command;
+import com.sun.lwuit.Component;
+import com.sun.lwuit.Container;
 import com.sun.lwuit.Display;
 import com.sun.lwuit.Form;
 import com.sun.lwuit.Label;
-import com.sun.lwuit.TabbedPane;
-import com.sun.lwuit.Tabs;
+import com.sun.lwuit.TextArea;
+import com.sun.lwuit.TextField;
+import com.sun.lwuit.animations.CommonTransitions;
+import com.sun.lwuit.animations.Transition;
 import com.sun.lwuit.events.ActionEvent;
 import com.sun.lwuit.events.ActionListener;
 import com.sun.lwuit.io.ConnectionRequest;
 import com.sun.lwuit.io.NetworkManager;
 import com.sun.lwuit.layouts.BorderLayout;
+import com.sun.lwuit.layouts.BoxLayout;
+import com.sun.lwuit.layouts.GridLayout;
 import com.sun.lwuit.plaf.UIManager;
 import com.sun.lwuit.util.Resources;
 
 public class AccountPanel implements ActionListener {
+	private boolean started;
+	private static final int EXIT_COMMAND = 1;
+	private static final int BACK_COMMAND = 3;
+	private static final Command exitCommand = new Command("Exit", EXIT_COMMAND);
+	private static final Command backCommand = new Command("Back", BACK_COMMAND);
 
 	public AccountPanel() {
 
@@ -32,45 +45,92 @@ public class AccountPanel implements ActionListener {
 		// TODO Auto-generated method stub
 	}
 
-	protected void startApp() {
-		String name = "TipsterTheme";
-		Resources r;
-		try {
-			r = Resources.open("/" + name + ".res");
-			UIManager.getInstance().setThemeProps(
-					r.getTheme(r.getThemeResourceNames()[0]));
+	protected void startApp(Hashtable bundle) {
+		if (!started) {
+			started = true;
+			String name = "theme";
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		Display.getInstance().callSerially(new Runnable() {
-			public void run() {
-				setMainForm();
+			try {
+				Resources r = Resources.open("/" + name + ".res");
+				UIManager.getInstance().setThemeProps(
+						r.getTheme(r.getThemeResourceNames()[0]));
+				UIManager.getInstance().setResourceBundle(bundle);
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		});
+			Display.getInstance().callSerially(new Runnable() {
+				public void run() {
+					showLoginForm();
+				}
+			});
+		}
 	}
 
-	protected void setMainForm() {
-		Form main = new Form();
-		main.setTitle("Hello World");
+	protected void showLoginForm() {
+		Form loginForm = new Form();
 
-		Tabs tab = new Tabs();
-		tab.addTab("Tab 1", new Label("Welcome to TabbedPane demo!"));
+		loginForm.setTitle("mainTitle");
+		loginForm.addComponent(new Label("loginName"));
+		final TextField searchFor = new TextField("cat", 50);
+		loginForm.addComponent(searchFor);
 
-		Form f = new Form();
+		loginForm.addComponent(new Label("password"));
+		final TextField password = new TextField("741258", 50);
+		password.setConstraint(TextArea.PASSWORD);
+		loginForm.addComponent(password);
 
-		//f.setLayout(new BorderLayout());
-		Label label = new Label("I am a Label");
-		f.addComponent( label);
+		Command login = new Command("login") {
+			public void actionPerformed(ActionEvent ae) {
+				showMainForm();
+			}
+		};
+		Button searchButton = new Button(login);
+		Container searchContainer = new Container(new BorderLayout());
+		searchContainer.addComponent(BorderLayout.EAST, searchButton);
+		loginForm.addComponent(searchContainer);
 
-		testConnectHttp(label);
-		tab.addTab("Tab 2", f);
+		loginForm.addCommand(exitCommand);
+		loginForm.addCommand(login);
+		loginForm.show();
+	}
 
-		main.addComponent(tab);
-		Command exitCommand = new Command("Exit");
-		main.addCommand(exitCommand);
-		main.addCommandListener(this);
-		main.show();
+	private void showMainForm() {
+		Form mainForm = new Form();
+		mainForm.setTitle("mainTitle");
+
+		Transition in, out;
+		out = CommonTransitions.createSlide(CommonTransitions.SLIDE_HORIZONTAL,
+				false, 500);
+		in = CommonTransitions.createSlide(CommonTransitions.SLIDE_HORIZONTAL,
+				true, 500);
+		mainForm.setTransitionInAnimator(in);
+		mainForm.setTransitionOutAnimator(out);
+
+		mainForm.setLayout(new BoxLayout(BoxLayout.Y_AXIS));
+
+		Button expenditure = createButton("expenditure");
+		expenditure.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				final BalancePane expenditure = new BalancePane("Expenditure");
+			}
+		});
+
+		mainForm.addComponent(expenditure);
+		mainForm.addComponent(createButton("income"));
+		mainForm.addComponent(createButton("query"));
+		mainForm.addComponent(createButton("overdraw"));
+
+		mainForm.addCommand(exitCommand);
+
+		mainForm.show();
+	}
+
+	private Button createButton(String string) {
+		Button button = new Button(string);
+		button.getUnselectedStyle().setAlignment(Label.CENTER);
+		button.getSelectedStyle().setAlignment(Label.CENTER);
+		button.getPressedStyle().setAlignment(Label.CENTER);
+		return button;
 	}
 
 	private void testConnectHttp(final Label label) {
@@ -92,8 +152,15 @@ public class AccountPanel implements ActionListener {
 		NetworkManager.getInstance().addToQueue(con);
 	}
 
-	public void actionPerformed(ActionEvent ae) {
-		Display.getInstance().exitApplication();
+	public void actionPerformed(ActionEvent evt) {
+		Command cmd = evt.getCommand();
+		switch (cmd.getId()) {
+		case EXIT_COMMAND:
+			Display.getInstance().exitApplication();
+			break;
+		case BACK_COMMAND:
+//			mainMenu.showBack();
+			break;
+		}
 	}
-
 }
--- mobile/src/cat/DesktopRun.java
+++ mobile/src/cat/DesktopRun.java
@@ -3,6 +3,7 @@ package cat;
 import java.awt.Frame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.Hashtable;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
@@ -11,6 +12,17 @@ import com.sun.lwuit.Display;
 import com.sun.lwuit.awtport.AwtImpl;
 
 public class DesktopRun {
+	private static Hashtable bundle = new Hashtable();
+	static {
+		bundle.put("mainTitle", "AccountForWife");
+		/*bundle.put("loginName", "LoginName");
+		bundle.put("password", "Password");
+		bundle.put("login", "Login");
+		bundle.put("exit", "Exit");
+		bundle.put("expenditure", "Expenditure");*/
+
+	}
+
 	private static void createAndShowGUI() {
 		Frame f = new Frame("run on desttop");
 		f.addWindowListener(new WindowAdapter() {
@@ -25,7 +37,7 @@ public class DesktopRun {
 		f.validate();
 		f.setLocationByPlatform(true);
 		f.setVisible(true);
-		new AccountPanel().startApp();
+		new AccountPanel().startApp(bundle);
 	}
 
 	public static void main(String[] args) {
