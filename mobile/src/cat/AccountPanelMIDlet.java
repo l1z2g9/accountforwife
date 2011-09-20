c66b56bab4653e6c9691b2260895a55e362157d0
querychinesesto <querychinesesto@localhost>
2011-09-20 18:24:54 +0800

    use lwuit

--- mobile/src/cat/AccountPanelMIDlet.java
+++ mobile/src/cat/AccountPanelMIDlet.java
@@ -1,15 +1,30 @@
 package cat;
 
+import java.util.Hashtable;
+
 import javax.microedition.midlet.MIDlet;
 
 import com.sun.lwuit.Display;
 
 public class AccountPanelMIDlet extends MIDlet {
 	private AccountPanel main = new AccountPanel();
+	private Hashtable bundle = new Hashtable();
+
+	public AccountPanelMIDlet() {
+		bundle.put("mainTitle", "小艺有数");
+		bundle.put("loginName", "用户名");
+		bundle.put("password", "密码");
+		bundle.put("login", "登录");
+		bundle.put("exit", "退出");
+		bundle.put("expenditure", "支出");
+		bundle.put("income", "收入");
+		bundle.put("query", "查询");
+		bundle.put("overdraw", "预支付");
+	}
 
 	public void startApp() {
 		Display.init(this);
-		main.startApp();
+		main.startApp(bundle);
 	}
 
 	public void pauseApp() {
