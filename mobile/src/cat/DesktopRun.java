93af4e5ee35b1c85fead416046cc42154126d9ad
querychinesesto <querychinesesto@localhost>
2011-11-01 18:43:40 +0800

    ''

--- mobile/src/cat/DesktopRun.java
+++ mobile/src/cat/DesktopRun.java
@@ -3,8 +3,12 @@ package cat;
 import java.awt.Frame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import javax.swing.SwingUtilities;
+
+import cat.model.Item;
 
 import com.sun.lwuit.Display;
 
@@ -27,11 +31,40 @@ public class DesktopRun {
 		new AccountPanel().startApp();
 	}
 
-	public static void main(String[] args) {
+	private static void run() {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				createAndShowGUI();
 			}
 		});
+	}
+
+	static void test() throws Exception {
+		Item item = new Item();
+		item.setAddress("addressXX");
+		item.setCategoryID("1000");
+		item.setMoney("12.5");
+		item.setTime("2011-10-29");
+		//		String content = "{date:\"2011-10-31\",category:1000:subCategory:1100}";
+		URL url = new URL("http://accountforwife.sinaapp.com/index.php/save");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		conn.getOutputStream().write(("item=" + item.toJson()).getBytes());
+		conn.getOutputStream().flush();
+
+		byte[] str = new byte[890000];
+		if (conn.getResponseCode() != 200) {
+			int b = conn.getErrorStream().read(str);
+			System.out.println("<<$$$ " + new String(str, 0, b));
+		} else {
+			int a = conn.getInputStream().read(str);
+			System.out.println("<< " + new String(str, 0, a));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		run();
+		//		test();
 	}
 }
