d7f45d9fff08b080edf68612d94d1ad630473f4b
querychinesesto <querychinesesto@localhost>
2011-11-04 10:02:50 +0800

    ''

--- mobile/src/cat/ComponentFactory.java
+++ mobile/src/cat/ComponentFactory.java
@@ -3,21 +3,25 @@ package cat;
 import java.util.Vector;
 
 import com.sun.lwuit.ComboBox;
+import com.sun.lwuit.Component;
 import com.sun.lwuit.Label;
 import com.sun.lwuit.TextField;
 import com.sun.lwuit.list.DefaultListModel;
+import com.sun.lwuit.plaf.Border;
 
 public class ComponentFactory {
 	public static Label getLabel(String msg) {
 		Label label = new Label(msg);
 		label.getStyle().setMargin(0, 0, 2, 0);
 		label.getStyle().setPadding(0, 0, 0, 0);
+		
 		return label;
 	}
 
 	public static TextField getTextField(int columns, int constraint) {
 		TextField field = new TextField(columns);
 		field.setConstraint(constraint);
+		
 		/*field.getStyle().setMargin(0, 0, 2, 0);
 		field.getStyle().setPadding(0, 0, 0, 0);
 		field.setPreferredW(40);
@@ -28,9 +32,14 @@ public class ComponentFactory {
 		return field;
 	}
 
+	private static void setBorder(Component c) {
+		c.getSelectedStyle().setBorder(Border.createLineBorder(2, 0xFFFF00));
+	}
+
 	public static ComboBox getCombox(Vector v) {
 		DefaultListModel model = new DefaultListModel(v);
 		ComboBox box = new ComboBox(model);
+		setBorder(box);
 		return alignComboBox(box);
 	}
 
